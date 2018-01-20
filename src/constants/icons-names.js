f48fdb4ed37ebc29c43f32a65b724edb46b0710d
Joseph Straub <josephrstraub@gmail.com>
2018-01-20 16:35:50 -0500

    add icons

---
+++ src/constants/icons-names.js
@@ -0,0 +1,17 @@
+export default [
+  'boomerang',
+  'campfire',
+  'cherries',
+  'chili',
+  'cone',
+  'football',
+  'guitar',
+  'hair-dryer',
+  'horse',
+  'light-bulb',
+  'money-bag',
+  'paint-brush',
+  'pint',
+  'snowflake',
+  'zebra'
+]
